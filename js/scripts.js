758f1818e57c07fe228c329c8b24b387f539b6dd
Paul Guevarra <p.a.guevarra@gmail.com>
2017-07-21 10:19:17 -0700

    create Pizza constructor and prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 //Business Logic
+function Pizza(size){
+  this.pSize = size;
+  this.pTopping = [];
+}
+
+Pizza.prototype.toCalc = function(){
+  return this.pSize + this.pTopping
+}
 
 
 
@@ -7,9 +15,14 @@ $(document).ready(function(){
   $("#blanks").submit(function(event){
     event.preventDefault();
     var inputName = $("input#name").val();
-
     var output = inputName
 
-    $("#receipt").text(output);
+    var inputSize = parseInt($("select#size").val());
+    var newPizza = new Pizza(inputSize)
+    // alert(newPizza);
+
+
+    $("#receipt").text(output + ",  your order costs $ " + newPizza.toCalc());
+
   });
 });
